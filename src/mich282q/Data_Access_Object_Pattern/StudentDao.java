339edfc86de8b60f3e242bbaf67e91ccf7cf6656
Michael Trans <mich282q@edu.easj.dk>
2018-10-08 18:03:20 +0200

    samlet java

---
+++ src/mich282q/Data_Access_Object_Pattern/StudentDao.java
@@ -0,0 +1,9 @@
+package mich282q.Data_Access_Object_Pattern;
+import java.util.List;
+
+public interface StudentDao {
+    public List<Student> getAllStudents();
+    public Student getStudent(int rollNo);
+    public void updateStudent(Student student);
+    public void deleteStudent(Student student);
+}
